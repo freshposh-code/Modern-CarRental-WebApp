c69123f64d9d34ffceda5267522769ff558af64a
freshposh-code <atomisefarouk919@gmail.com>
2024-09-20 20:29:24 +0100

    "HandleSubmit for login"

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import { AuroraBackground } from "../components/ui/aurora-background";
 import Image from "next/image";
 import { BackgroundGradient } from "@/components/ui/background-gradient";
@@ -10,12 +10,16 @@ import { CiLock } from "react-icons/ci";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import Link from "next/link";
+import { useDispatch } from "react-redux";
+import { setLoading } from "@/Redux/features/LoadingSlice";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 interface FormData {
     name: string;
     email: string;
-    password: string | number;
-    confirmPassword: string | number;
+    password: string;
+    confirmPassword: string;
 }
 
 const login = () => {
@@ -37,9 +41,50 @@ const login = () => {
             [name]: value,
         }));
     };
-    console.log(data)
+
+    const dispatch = useDispatch()
+    const router = useRouter()
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        dispatch(setLoading(true))
+
+        try {
+            const response = await fetch('/api/userSignIn', {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+
+            const userResponse: { message?: String, error?: String, success?: String } = await response.json()
+
+            dispatch(setLoading(false))
+
+            if (response.ok) {
+                toast.success(userResponse.message || "Signed in successfully")
+                router.push('/')
+            }
+            else {
+                toast.error(userResponse.message || "Something went wrong!")
+            }
+
+        } catch (error) {
+            console.log(error)
+            dispatch(setLoading(false))
+            toast.error("An unexpected error occurred, please try again.")
+        }
+    }
+
     return (
         <AuroraBackground>
+            <Link href='/'>
+                <div className='flex items-center gap-2 absolute top-5 left-5'>
+                    <div className="border-4 border-blue-700 sm:size-12 size-8 rounded-full" />
+                    <h1 className='font-extrabold sm:text-3xl text-lg text-black-100 dark:text-slate-100'>Rivent</h1>
+                </div>
+            </Link>
             <motion.div
                 initial={{ opacity: 0.0, y: 40 }}
                 whileInView={{ opacity: 1, y: 0 }}
@@ -51,20 +96,20 @@ const login = () => {
                 className="z-20"
             >
                 <BackgroundGradient>
-                    <form className="flex justify-center items-center bg-zinc-50 dark:bg-zinc-800 dark:text-slate-100 rounded-[22px] p-3 relative">
+                    <form className="flex justify-center items-center bg-zinc-100 dark:bg-zinc-800 dark:text-slate-100 rounded-[22px] p-3 relative" onSubmit={handleSubmit}>
                         <div className="flex flex-col w-full sm:min-w-[370px] max-w-[] sm:mt-14 mt-4">
                             <div>
                                 <Image className="absolute -top-20" src='/GTpng.png' alt="GTpng" width={1000} height={1000} />
                             </div>
                             <label className="font-medium">Email</label>
-                            <div className="flex items-center bg-slate-100 dark:bg-zinc-600 my-1 rounded-[22px]">
+                            <div className="flex items-center bg-slate-200 dark:bg-zinc-600 my-1 rounded-[22px]">
                                 <MdOutlineMailOutline className="text-[32px] pl-3" />
-                                <input className="outline-none w-full bg-transparent p-3" type="email" name="email" value={data.email} onChange={handleOnChange} placeholder="Email address" />
+                                <input className="outline-none w-full bg-transparent p-3" type="email" name="email" value={data.email} onChange={handleOnChange} placeholder="Email address" required />
                             </div>
                             <label className="font-medium">Password</label>
-                            <div className="flex items-center bg-slate-100 dark:bg-zinc-600 my-1 rounded-[22px] cursor-pointer">
+                            <div className="flex items-center bg-slate-200 dark:bg-zinc-600 my-1 rounded-[22px] cursor-pointer">
                                 <CiLock className="text-[32px] pl-3" />
-                                <input className="outline-none w-full bg-transparent p-3" type={passswordIcon ? "text" : "password"} name="password" value={data.password} onChange={handleOnChange} placeholder="Password" />
+                                <input className="outline-none w-full bg-transparent p-3" type={passswordIcon ? "text" : "password"} name="password" value={data.password} onChange={handleOnChange} placeholder="Password" required />
                                 <span onClick={() => setPassswordIcon((prev) => !prev)}>
                                     {
                                         passswordIcon ?
@@ -74,7 +119,7 @@ const login = () => {
                             </div>
                             <button className="bg-blue-600 font-bold text-slate-50 w-full rounded-[22px] py-3 my-2">Login In</button>
 
-                            <Link href={''}>
+                            <Link href='/forgotPassword'>
                                 <p className="text-xs text-red-400 text-right font-bold">forgot password?</p>
                             </Link>
 
