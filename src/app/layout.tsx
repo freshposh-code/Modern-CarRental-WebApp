a5c2a78ab97e4a9b6c2068b37d6ff03c20692981
freshposh-code <atomisefarouk919@gmail.com>
2024-09-23 02:17:51 +0100

    "modified styles and layout"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/Theme/theme-provider";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata: Metadata = {
   title: "Car Rental",
@@ -14,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="dark:bg-darkBackground">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -23,6 +25,15 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <ToastContainer
+          position="top-center"
+          toastStyle={{
+            backgroundColor: '#868686',
+            color: '#fff',
+            borderRadius: '22px',
+            fontSize: '14px',
+          }}
+        />
       </body>
     </html>
   );
