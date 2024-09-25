2f42c6480ae473de7967ce0289d3277e0f8d1d7a
freshposh-code <atomisefarouk919@gmail.com>
2024-09-25 03:06:52 +0100

    "set Admin/dashboard and wrap app with provider"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+'use client'
+
+import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import { UserProvider } from "@/context/UserContext";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <div className="hero-gradient dark:bg-gradient-to-b from-[rgba(182,195,255,0.85)] via-[#5860a7] to-[#202754] contrast-200 h-screen rounded-b-[35rem]">
+        <UserProvider>
+          <Navbar />
+          <HeroSection />
+
+        </UserProvider>
+      </div>
+
+    </>
   );
 }
