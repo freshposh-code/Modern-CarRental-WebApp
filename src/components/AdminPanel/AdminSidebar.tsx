11a54b8e3731c038023fa0663d5151345a16f8e4
freshposh-code <atomisefarouk919@gmail.com>
2024-10-03 13:47:24 +0100

    "Update admin sidebar"

---
+++ src/components/AdminPanel/AdminSidebar.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import React, { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "../ui/sidebar";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { links } from "@/utils/Data";
+
+export function AdminSidebar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div
+      className={cn(
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        "h-screen"
+      )}
+    >
+      <Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className="justify-between gap-10">
+          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            {open ? <Logo /> : <LogoIcon />}
+            <div className="mt-8 flex flex-col gap-2">
+              {links.map((link, idx) => (
+                <SidebarLink key={idx} link={link} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <SidebarLink
+              link={{
+                label: "Posh Code",
+                href: "#",
+                icon: (
+                  <Image
+                    src="/GTpng.png"
+                    className="h-7 w-7 object-cover rounded-full"
+                    width={50}
+                    height={50}
+                    alt="Avatar"
+                  />
+                ),
+              }}
+            />
+          </div>
+        </SidebarBody>
+      </Sidebar>
+    </div>
+  );
+}
+export const Logo = () => {
+  return (
+    <Link
+      href="/"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-black dark:text-white whitespace-pre"
+      >
+        <div className='flex items-center gap-2 z-[1000]'>
+                    <div className="border-4 border-blue-700 sm:size-10 size-8 rounded-full" />
+                    <h1 className='font-extrabold sm:text-3xl text-2xl'>Rivent</h1>
+                </div>
+      </motion.span>
+    </Link>
+  );
+};
+export const LogoIcon = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+        <div className="border-4 border-blue-700 sm:size-7 size-5 rounded-full" />
+    </Link>
+  );
+};
