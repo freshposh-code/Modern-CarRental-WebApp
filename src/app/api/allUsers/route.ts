a538ad71181ff0dca752d41dab24529fb2ab7d66
freshposh-code <atomisefarouk919@gmail.com>
2024-10-06 12:23:11 +0100

    set API call to get all users

---
+++ src/app/api/allUsers/route.ts
@@ -0,0 +1,25 @@
+// src/app/api/allUsers/route.ts
+import userModel from '@/server/models/userModel';
+import { connectDB } from '@/server/MongooseConnect';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    try {
+        await connectDB();
+
+        // Fetch users, excluding their passwords
+        const users = await userModel.find().select('-password');
+
+        return NextResponse.json({
+            message: 'All users retrieved successfully',
+            data: users,
+            success: true,
+        });
+    } catch (err: any) {
+        return NextResponse.json({
+            message: err.message || 'Internal Server Error',
+            error: true,
+            success: false,
+        }, { status: 500 });
+    }
+}
