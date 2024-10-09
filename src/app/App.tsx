6d78d8ce542d98890e1aa066134824baef7693ee
freshposh-code <atomisefarouk919@gmail.com>
2024-10-10 00:19:44 +0100

    set userProvider to work for app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,16 @@
 'use client';
 
+import { UserProvider } from '@/context/UserContext';
 import { store } from '@/Redux/store';
 import { Provider } from 'react-redux'
 
 const App = ({ children }: { children: React.ReactNode }) => {
     return (
+        <UserProvider>
         <Provider store={store}>
             {children}
         </Provider>
+        </UserProvider>
     )
 }
 
