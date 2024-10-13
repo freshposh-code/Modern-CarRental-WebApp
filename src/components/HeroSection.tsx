e77c93bc890da9b9aa35783e8548afad7a1d338d
freshposh-code <atomisefarouk919@gmail.com>
2024-10-14 00:36:29 +0100

    commit changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -25,7 +25,7 @@ export default function HeroSection() {
                     initial="hidden"
                     whileInView="show"
                     variants={fadeIn('down', 'tween', 0.70, 0.75)}
-                    className='bg-white dark:bg-zinc-800 w-fit flex md:flex-row flex-col justify-center items-center m-auto my-7 gap-10 p-7 rounded-[50px]'>
+                    className='bg-white dark:bg-zinc-800 w-fit flex xl:flex-row flex-col justify-center items-center m-auto my-7 gap-10 p-7 rounded-[50px]'>
                     <div className='flex items-center gap-4'>
                         <div>
                             <MdOutlineLocationOn className='text-2xl' />
@@ -76,10 +76,9 @@ export default function HeroSection() {
                  initial="hidden"
                  whileInView="show"
                  variants={fadeIn('up', 'tween', 0.70, 0.75)}
-                className="flex justify-center items-center md:-mt-[8.4rem] -mt-[3rem] hero-img ">
-                    <Image src='/sideView.png' alt='carSideView' width={1000} height={1000} className="w-[1000px] object-contain" />
+                className="flex justify-center items-center md:-mt-[8.4rem] -mt-[3rem]">
+                    <img src='/sideView.png' alt='carSideView' width={1000} height={1000} className="w-[1000px] object-contain" />
                 </motion.div>
-
             </section>
         </>
     )
