d54cd0a3b86c9a3a341c78aa3462d4313350b755
freshposh-code <atomisefarouk919@gmail.com>
2024-10-14 00:39:22 +0100

    Add key to mapping

--- src/components/DisplayLogo.tsx
+++ src/components/DisplayLogo.tsx
@@ -7,7 +7,9 @@ const DisplayLogo = () => {
     <>
     <section className='flex flex-wrap justify-between items-center gap-5'>
         {displaylogo.map((item) => (
+          <div key={item.logo}>
             <Image src={item.logo} alt='logo' width={500} height={500} className="md:size-40 size-28 object-contain m-auto" />
+          </div>
         ))}
     </section>
     </>
