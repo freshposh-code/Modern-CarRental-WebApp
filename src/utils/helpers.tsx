8081e298e1a1cbe21d45c8fba4df3d7b1cce43c0
freshposh-code <atomisefarouk919@gmail.com>
2024-10-19 01:37:37 +0100

    set CLOUDINARY logic for upload

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,18 +1,45 @@
-import { toast } from 'react-toastify';
+const url = `https://api.cloudinary.com/v1_1/dzsmwdpk7/image/upload`;
 
-const toastOptions = {
-    style: {
-        background: '#868686',
-        color: '#fff',
-        borderRadius: '22px',
-        fontSize: '16px',
-    },
-};
+interface UploadResponse {
+  asset_id: string;
+  public_id: string;
+  version: number;
+  version_id: string;
+  signature: string;
+  width: number;
+  height: number;
+  format: string;
+  resource_type: string;
+  created_at: string;
+  tags: string[];
+  bytes: number;
+  type: string;
+  etag: string;
+  placeholder: boolean;
+  url: string;
+  secure_url: string;
+  original_filename: string;
+}
 
-const makeToast = {
-    success: (text: string) => toast.success(text, toastOptions),
-    error: (text: string) => toast.error(text, toastOptions),
-    warning: (text: string) => toast.warning(text, toastOptions),
-};
+const uploadImage = async (image: File): Promise<UploadResponse> => {
+    const formData = new FormData();
+    formData.append("file", image);
+    formData.append("upload_preset", "car_rental");
+  
+    const dataResponse = await fetch(url, {
+      method: "POST",
+      body: formData
+    });
+  
+    const result = await dataResponse.json();
+    console.log(result);
+  
+    if (!dataResponse.ok) {
+      throw new Error(`Failed to upload image: ${result.error?.message || 'Unknown error'}`);
+    }
+  
+    return result;
+  };
+  
 
-export default makeToast;
+export default uploadImage;
