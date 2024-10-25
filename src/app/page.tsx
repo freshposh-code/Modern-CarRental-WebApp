dec163b4bfceffb99151e367f8ea84527f83aa1b
freshposh-code <atomisefarouk919@gmail.com>
2024-10-25 01:58:58 +0100

    commit client App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,13 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <>
-      <div className="hero-gradient dark:bg-gradient-to-b from-[rgba(182,195,255,0.85)] via-[#5860a7] to-[#202754] contrast-200 h-screen rounded-b-[35rem]">
+      <div className="hero-gradient dark:bg-gradient-to-b from-[rgba(182,195,255,0.85)] via-[#5860a7] to-[#202754] contrast-200 lg:h-screen h-auto rounded-b-[35rem]">
           <Navbar />
           <HeroSection />
-          <div className="px-16">
+      </div>
+      <div className="md:px-16 px-6 xl:mt-[10rem] mt-0">
           <DisplayLogo />
           </div>
-      </div>
-
     </>
   )
 }
