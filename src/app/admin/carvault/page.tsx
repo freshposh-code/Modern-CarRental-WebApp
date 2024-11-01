0b79fb7576acce4eaf8aa0cde7df8074a9e7844a
freshposh-code <atomisefarouk919@gmail.com>
2024-11-01 20:27:13 +0100

    set admin layout for carVault

--- src/app/admin/carvault/page.tsx
+++ src/app/admin/carvault/page.tsx
@@ -1,13 +1,57 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoCarSportSharp } from "react-icons/io5";
 import { FaCloudUploadAlt } from "react-icons/fa";
 import UploadCar from "@/components/AdminPanel/UploadCar";
 import VaultCard from "@/components/AdminPanel/VaultCard";
+import { useDispatch } from "react-redux";
+import { setLoading } from "@/Redux/features/LoadingSlice";
+import { toast } from "react-toastify";
+
+export interface Vaultdata {
+  _id: string;
+capacity: string;
+carImage: string[];
+carName: string;
+carType: string;
+category: string;
+description: string;
+passengers : string;
+price: number
+transmission: string;
+}
 
 const Page = () => {
-  const [upload, setUpload] = useState(false)
+  const [upload, setUpload] = useState(false);
+
+  const [data, setData] = useState<Vaultdata[]>([]);
+  const dispatch = useDispatch()
+
+  const fetchCars = async () => {
+      try {
+          dispatch(setLoading(true));
+          const fetchData = await fetch('/api/getVault',{
+           method: 'GET',
+          credentials: 'include',
+          })
+          const dataApi = await fetchData.json();
+          dispatch(setLoading(false));
+          console.log('allCars', dataApi)
+
+          if(dataApi.success) {
+              setData(dataApi.data)
+          }
+          if(dataApi.error) {
+              toast.error(dataApi.message)
+          }
+      } catch (error) {
+          console.log(error)
+      }
+}
+      useEffect(() => {
+          fetchCars()
+      }, [])
   return (
     <>
     <div className='sm:my-5 my-2 sm:mx-5 mx-2 w-full bg-neutral-100 dark:bg-zinc-800 h-[calc(100vh-40px)] rounded-xl overflow-y-auto'>
@@ -23,11 +67,11 @@ const Page = () => {
       </span>
       </div>
       <div className="m-3">
-      <VaultCard />
+      <VaultCard data={data} fetchCars={fetchCars} />
       </div>
    </div>
       {
-        upload && <UploadCar onClose={() => setUpload(false)} />
+        upload && <UploadCar onClose={() => setUpload(false)}  fetchCars={fetchCars} />
       }
       </>
   )
