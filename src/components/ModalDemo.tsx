bbe99bd1e4f114e582d0ff55d36137580f05b9e0
freshposh-code <atomisefarouk919@gmail.com>
2024-12-02 13:00:27 +0100

    update viewPort

--- src/components/ModalDemo.tsx
+++ src/components/ModalDemo.tsx
@@ -20,7 +20,7 @@ export function AnimatedModalDemo({data}: ItemsData) {
     <div className="flex items-center justify-center">
       <Modal>
         <ModalTrigger className="bg-blue-700 rounded-full text-white flex justify-center group/modal-btn">
-          <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500 md:text-base text-[9px]">
+          <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500 lg:text-base xm:text-[9px] text-[6px]">
             Rent Now
           </span>
           <div className="-translate-x-40 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20">
