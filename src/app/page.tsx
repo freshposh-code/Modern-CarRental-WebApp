cb011c8f8394451dad481b8a684ac1ae7fac26f5
freshposh-code <atomisefarouk919@gmail.com>
2024-12-09 03:16:05 +0100

    `Added BestSection, Journey components and updated Home page layout`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import BestSection from "@/components/BestSection";
 import DisplayLogo from "@/components/DisplayLogo";
 import HeroSection from "@/components/HeroSection";
+import Journey from "@/components/Journey";
 import Navbar from "@/components/Navbar";
 import PopularSection from "@/components/PopularSection";
 
@@ -16,6 +18,10 @@ export default function Home() {
           <DisplayLogo />
 
           <PopularSection category='popular' title='Our Popular Car' desc='Enjoy exclusive deals and the best prices for satisfying travel packages. We offer the best value for everyday adventure.' />
+
+          <BestSection category='best' title='The Best Platform for Car Rental' desc='Enjoy exclusive deals and the best prices for satisfying travel packages. We offer the best value for everyday adventure.'/>
+
+          <Journey title='Embark on a journey and Performance' desc='Enjoy exclusive deals and the best prices for satisfying travel packages. We offer the best value for everyday adventure.'/>
           </div>
     </>
   )
