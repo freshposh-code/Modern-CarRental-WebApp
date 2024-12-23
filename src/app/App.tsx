3e3573913f0b2b511f35e1b03fb50ad5ccb84f88
freshposh-code <atomisefarouk919@gmail.com>
2024-12-23 21:13:23 +0100

    Wrap WishlistProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { UserProvider } from '@/context/UserContext';
+import { WishlistProvider } from '@/context/WishlistContext';
 import { store } from '@/Redux/store';
 import { Provider } from 'react-redux'
 
 const App = ({ children }: { children: React.ReactNode }) => {
     return (
         <UserProvider>
+            <WishlistProvider>
         <Provider store={store}>
             {children}
         </Provider>
+        </WishlistProvider>
         </UserProvider>
     )
 }
