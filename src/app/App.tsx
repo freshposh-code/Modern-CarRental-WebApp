0524d542c4a8763e36c4b80a17d5ec8bfe77a227
freshposh-code <atomisefarouk919@gmail.com>
2025-02-18 20:33:55 +0100

    `Removed redundant Provider component in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,15 +8,15 @@ import { Provider } from 'react-redux'
 
 const App = ({ children }: { children: React.ReactNode }) => {
     return (
+        <Provider store={store}>
         <UserProvider>
                  <BookingProvider>
             <WishlistProvider>
-        <Provider store={store}>
             {children}
-        </Provider>
           </WishlistProvider>
                 </BookingProvider>
         </UserProvider>
+        </Provider>
     )
 }
 
