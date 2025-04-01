374ca317bd5b7ba329496b4d21701af2b6f08b58
freshposh-code <atomisefarouk919@gmail.com>
2025-04-01 01:31:58 +0100

    `feat: updated various components and models with new features and improvements`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,12 @@ export default function Home() {
       const isLoading = useAppSelector(store => store.loadingSlice);
   return (
     <>
-      <div className="hero-gradient dark:bg-gradient-to-b from-[rgba(182,195,255,0.85)] via-[#5860a7] to-[#202754] contrast-200 lg:h-screen h-auto rounded-b-[35rem]">
+      <div className="hero-gradient dark:bg-gradient-to-b from-[rgba(182,195,255,0.85)] via-[#5860a7] to-[#202754] contrast-200 lg:h-screen h-auto rounded-b-[35rem] ">
           <Navbar />
           <HeroSection />
 
       </div>
-      <div className="md:px-16 px-6 xl:mt-[10rem] mt-0">
+      <div className="md:px-16 px-6 xl:mt-[10rem] mt-0 relative">
           <DisplayLogo />
 
           <PopularSection category='popular' title='Our Popular Car' desc='Enjoy exclusive deals and the best prices for satisfying travel packages. We offer the best value for everyday adventure.' />
@@ -32,7 +32,9 @@ export default function Home() {
           <div className="mt-16">
         <Footer/>
         </div>
+        <div className="sticky bottom-5">
         <BookingButton />
+        </div>
         {isLoading && <Loader />}
     </>
   )
--- src/components/Client/BookingButton.tsx
+++ src/components/Client/BookingButton.tsx
@@ -12,21 +12,24 @@ export default function BookingButton() {
     return (
         <>
 
-        <div className={`sticky bottom-5 top-5 m-4 float-right cursor-pointer z-[2000] ${clickBookings ? 'hidden' : 'flex'}`}>
+        <div className={`absolute bottom-0 right-3 cursor-pointer z-[2000] ${clickBookings ? 'hidden' : 'flex'}`}>
                 <div 
-                    className="bg-blue-600/65 text-white size-12 rounded-full text-2xl flex justify-center items-center" 
+                    className="bg-blue-600/65 text-white size-12 rounded-full text-2xl flex justify-center items-center active:scale-110 transition-all duration-150" 
                     onClick={() => setClickBookings((prev:any) => !prev)}
                 >
                     <IoCarSportSharp />
                 </div>
             </div>
 
-            <div>
+            <div
+            className={`fixed inset-0 z-50 transition-transform duration-500 ease-in-out ${
+                clickBookings ? "opacity-100 visible translate-y-0 duration-500"
+            : "opacity-0 invisible translate-y-8 duration-500"
+              }`}>
                 {clickBookings && (
-                    <ShowBookings 
-                        state={clickBookings} 
+                    <ShowBookings  
                         setState={() => setClickBookings(false)} 
-                        data={data} 
+                        data={data}  
                         countBookings={countBookings} 
                         item={selectedItem || undefined} 
                         setSelectedItem={setSelectedItem}
